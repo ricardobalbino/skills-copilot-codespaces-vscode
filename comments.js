719bad80711ad1ab68f3dc34960497a505530363
ricardobalbino <balbino.ricardo@gmail.com>
2025-02-06 13:17:01 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Create we server
+const express = require('express');
+const app = express();
+
+// Create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
